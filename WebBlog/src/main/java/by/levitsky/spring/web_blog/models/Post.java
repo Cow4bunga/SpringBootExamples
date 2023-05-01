3997da0b5c751c57342baebf5f7b9f0a03eb7596
Ivan L <levitskyi.1002@gmail.com>
2023-05-01 22:55:49 +0300

    removed validators for now

--- WebBlog/src/main/java/by/levitsky/spring/web_blog/models/Post.java
+++ WebBlog/src/main/java/by/levitsky/spring/web_blog/models/Post.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Getter
@@ -20,16 +18,13 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @NotEmpty(message = "Title shouldn't be empty!")
-    @Size(min = 2, max = 64, message = "Between 2 and 64 characters!")
+
     private String title;
-    @NotEmpty(message = "Preview shouldn't be empty!")
-    @Size(min = 2, max = 64, message = "Between 2 and 64 characters!")
+
     private String anons;
-    @NotEmpty(message = "Text shouldn't be empty!")
-    @Size(min = 2, max = 2000, message = "Between 2 and 2000 characters!")
+
     private String full_text;
-    private int views;
+    private int views=0;
 
     public Post(String title, String anons, String full_text) {
         this.title = title;
